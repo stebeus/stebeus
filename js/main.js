8f28b1a2aa555ff2ad5b3224f5205c76bbbbf97c
stebeus <stebeus@mailfence.com>
2025-10-09 07:35:43 -0300

    Remove the original event listener algorithm

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@
 // Query select the document element with ID 'btn-theme' in the constant 'themeToggle'
 const themeToggle = document.querySelector('#btn-theme');
 
-// Add a click event listener to it
-
 // Add click event listener to 'themeToggle' that toggles dark mode on the body
 themeToggle.addEventListener('click', (e) => {
   document.querySelector('body').classList.toggle('darkmode');
