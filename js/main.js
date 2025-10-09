49f6c9dc2c92bddd46cdfdb979ca51d994e5b136
stebeus <stebeus@mailfence.com>
2025-10-09 09:35:49 -0300

    Write dark mode algorithm disclaimer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 'use strict';
 
+/**
+ * ! The current dark mode algorithm is event listener based, so it does not work with automatic theme detection
+ * TODO: Replace it with a function-based algorithm
+ */
+
 // Query select the document element with ID 'btn-theme' in the constant 'themeToggle'
 const themeToggle = document.querySelector('#btn-theme');
 
