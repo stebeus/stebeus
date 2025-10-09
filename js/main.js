c564411d192bc9d0e4b4a0a4a28800a3631a49e3
stebeus <stebeus@mailfence.com>
2025-10-09 06:49:23 -0300

    Create the dark theme toggle pseudocode

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// Query select the document element with ID 'btn-theme'
+
+// Add a click event listener to it
+
+/* 
+WHEN user clicks btn-theme:
+  IF current theme is light THEN
+    add 'darkmode' class to body
+  ELSE
+    remove 'darkmode' class from body
+  ENDIF
+*/
+
+// Store updated theme preference in local storage
