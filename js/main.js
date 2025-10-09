a048cf43f326f21ee00409e37d246c5be90671d6
stebeus <stebeus@mailfence.com>
2025-10-09 08:22:30 -0300

    Add body selection constant

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@
 // Query select the document element with ID 'btn-theme' in the constant 'themeToggle'
 const themeToggle = document.querySelector('#btn-theme');
 
+// Simplify document selection with constant 'body'
+const body = document.querySelector('body');
+
 // Add click event listener to 'themeToggle' that toggles dark mode on the body
 themeToggle.addEventListener('click', (e) => {
   document.querySelector('body').classList.toggle('darkmode');
