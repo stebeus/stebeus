c796a2119c2062744b2649c275f9f4c07151ff12
stebeus <stebeus@mailfence.com>
2025-10-09 06:54:13 -0300

    Include constant assignment in the query select algorithm

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Query select the document element with ID 'btn-theme'
+// Query select the document element with ID 'btn-theme' in the constant 'themeToggle'
 
 // Add a click event listener to it
 
