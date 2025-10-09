86e39202d2b58ff4f79ebb08717207cea0c5e6c8
stebeus <stebeus@mailfence.com>
2025-10-09 08:28:02 -0300

    Create page load local theme algorithm

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ const themeToggle = document.querySelector('#btn-theme');
 // Simplify document query selection with constant 'body'
 const body = document.querySelector('body');
 
+// On page load, check local storage and apply theme
+
 // Add click event listener to 'themeToggle' that toggles dark mode on the body
 themeToggle.addEventListener('click', (e) => {
   body.classList.toggle('darkmode');
